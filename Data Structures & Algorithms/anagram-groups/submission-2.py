beef2e8a0eb4ad62afe2f8044b343b587518019f
Yuvraj <saheb.yuvraj@gmail.com>
2026-08-04 10:11:39 +1000

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        
+        grps = defaultdict(list)
+
+        for word in strs:
+            sorted_word = "".join(sorted(word))
+            if sorted_word in grps:
+                grps[sorted_word].append(word)
+            else:
+                grps[sorted_word] = [word]
+
+        return [words for words in grps.values()]
+
+            
